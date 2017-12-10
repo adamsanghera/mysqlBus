6bff242620048d3133118e6c2c0781a062e6b29b
Adam <as8513@nyu.edu>
2017-12-09 20:10:23 -0500

    fixed bug

--- init.go
+++ init.go
@@ -17,7 +17,11 @@ var (
 
 func init() {
 	var err error
-	DB, err = sql.Open("mysql", username+":"+password+"@"+protocol+"("+address+":"+port+")/app?parseTime=true")
+	if len(password) != 0 {
+		DB, err = sql.Open("mysql", username+":"+password+"@"+protocol+"("+address+":"+port+")/app?parseTime=true")
+	} else {
+		DB, err = sql.Open("mysql", username+"@"+protocol+"("+address+":"+port+")/app?parseTime=true")
+	}
 	if err != nil {
 		panic(err)
 	}
